f2bf566857e96b55b382f9f81285a8e99cb881dc
Emre Kanatli <kanatliemre@gmail.com>
2020-07-06 19:33:25 -0400

    filename updated to UTC date

--- src/components/DownloadKeyfile.vue
+++ src/components/DownloadKeyfile.vue
@@ -123,7 +123,9 @@
             let text = JSON.stringify(file_data);
 
 
-            let filename = `AVAX_${addr.split('-')[1]}`;
+            let utcDate = new Date()
+            let dateString = utcDate.toISOString().replace(' ', '_');
+            let filename = `AVAX_${dateString}.json`;
 
             var blob = new Blob(
                 [ text ],
@@ -141,113 +143,6 @@
             document.body.removeChild(element);
         }
     }
-    //
-    // export default {
-    //     data(){
-    //         return {
-    //             password: "",
-    //             confirmPass: "",
-    //             isLoad: false,
-    //         }
-    //     },
-    //     props: {
-    //         master_keypair: Object,
-    //     },
-    //     components: {
-    //         // KeyCard,
-    //         Spinner
-    //     },
-    //     computed: {
-    //         err(){
-    //             if(!this.password) return "";
-    //
-    //             if(this.password.length < 9) return "Password must be at least 9 characters long.";
-    //             if(this.password != this.confirmPass) return "Passwords do not match.";
-    //
-    //             return "";
-    //         },
-    //         canSubmit(){
-    //             let pass = this.password;
-    //
-    //             if(pass.length < 9){
-    //                 return false;
-    //             }
-    //
-    //             if(pass !== this.confirmPass){
-    //                 return false;
-    //             }
-    //             return true;
-    //         }
-    //     },
-    //     methods: {
-    //         submit(){
-    //             let parent = this;
-    //             this.isLoad = true;
-    //
-    //             setTimeout(async ()=> {
-    //                 await parent.exportKey();
-    //                 parent.isLoad = false;
-    //                 parent.oncomplete();
-    //             }, 1000);
-    //         },
-    //         oncomplete(){
-    //             this.$emit('complete');
-    //         },
-    //
-    //         async exportKey(){
-    //             let pass = this.password;
-    //             let salt = await cryptoHelpers.makeSalt();
-    //             let passHash = await cryptoHelpers.pwhash(pass, salt);
-    //
-    //
-    //             // Loop private keys, encrypt them and store in an array
-    //             let key = this.master_keypair;
-    //             let pk = key.getPrivateKey();
-    //             let addr = key.getAddressString();
-    //
-    //             let pk_crypt = await cryptoHelpers.encrypt(pass,pk,salt);
-    //             let key_data = {
-    //                 key: bintools.avaSerialize(pk_crypt.ciphertext),
-    //                 iv: bintools.avaSerialize(pk_crypt.iv),
-    //                 address: addr,
-    //             };
-    //
-    //             let keys = [key_data];
-    //
-    //             const KEYSTORE_VERSION = '2.0';
-    //
-    //
-    //             let file_data = {
-    //                 version: KEYSTORE_VERSION,
-    //                 salt: bintools.avaSerialize(salt),
-    //                 pass_hash: bintools.avaSerialize(passHash.hash),
-    //                 keys: keys,
-    //             };
-    //
-    //             // Download the file
-    //
-    //             let text = JSON.stringify(file_data);
-    //
-    //
-    //             let filename = `AVAX_${addr.split('-')[1]}`;
-    //
-    //             var blob = new Blob(
-    //                 [ text ],
-    //                 {
-    //                     type : "application/json"
-    //                 }
-    //             );
-    //             let url = URL.createObjectURL( blob );
-    //             var element = document.createElement('a');
-    //             element.setAttribute('href', url);
-    //             element.setAttribute('download', filename);
-    //             element.style.display = 'none';
-    //             document.body.appendChild(element);
-    //             element.click();
-    //             document.body.removeChild(element);
-    //         }
-    //     }
-    // }
 </script>
 <style scoped lang="scss">
     $blursize: 0.2rem;
